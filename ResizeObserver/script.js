d308778c10047b315f4e61a192b129fab0b31e0c
Roshni V R <vrroshni2001@gmail.com>
2023-09-09 19:49:36 +0530

    resize observer done

---
+++ ResizeObserver/script.js
@@ -0,0 +1,12 @@
+const box = document.querySelector('.box')
+const randomcolor = () => {
+    for (let i = 0; i < 10; i++)
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
+const observer = new ResizeObserver(entries => {
+    const target = entries[0]
+    const isSmall = target.contentRect.width < 450
+    target.target.style.backgroundColor = isSmall ? randomcolor() : "red"
+})
+
+observer.observe(box)
